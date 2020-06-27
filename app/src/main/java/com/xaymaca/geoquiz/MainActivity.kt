836804aeeb20a12fdff87e4e2ec33ed7acb73c61
Vincent Stoessel <vincents@backbase.com>
2020-06-27 19:07:06 -0400

    challenge to display toast on top using gravity

--- app/src/main/java/com/xaymaca/geoquiz/MainActivity.kt
+++ app/src/main/java/com/xaymaca/geoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.xaymaca.geoquiz
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -18,13 +19,21 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener { view : View ->
-        Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        trueButton.setOnClickListener { view: View ->
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this,
-                R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+
+            val toast = Toast.makeText(
+                this, R.string.incorrect_toast, Toast.LENGTH_SHORT
+            )
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+
+
         }
     }
 }
