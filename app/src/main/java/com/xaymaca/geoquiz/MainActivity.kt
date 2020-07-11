9de6a1b390098d7a9b6b177084ee07b2debe6868
Vincent Stoessel <vincents@backbase.com>
2020-07-11 09:46:32 -0400

    add left button and learned about drawableEnd vs drawableLeft

--- app/src/main/java/com/xaymaca/geoquiz/MainActivity.kt
+++ app/src/main/java/com/xaymaca/geoquiz/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.xaymaca.geoquiz
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -12,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var previousButton: Button
     private lateinit var questionTextView: TextView
 
 
@@ -33,19 +33,31 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previousButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
 
 
-        trueButton.setOnClickListener { view: View ->
+
+        questionTextView.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        trueButton.setOnClickListener {
             checkAnswer(true)
         }
 
-        falseButton.setOnClickListener { view: View ->
+        falseButton.setOnClickListener {
             checkAnswer(false)
         }
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
             updateQuestion()
         }
         updateQuestion()
